5fdb8da74b9b7238a3ab13b4412a48ce7e376c99
iccet <hr.donev@gmail.com>
2021-01-14 22:48:53 +0200

    Create config server

---
+++ src/main/java/bg/mvr/dcis/congifserver/CongifServerApplication.java
@@ -0,0 +1,15 @@
+package bg.mvr.dcis.congifserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CongifServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CongifServerApplication.class, args);
+    }
+
+}
