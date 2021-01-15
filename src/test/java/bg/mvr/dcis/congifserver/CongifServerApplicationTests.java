2eb4c4864a6c51e8824c8fa368aea53a7e9b487c
iccet <hr.donev@gmail.com>
2021-01-15 09:32:07 +0200

    Create config server

---
+++ src/test/java/bg/mvr/dcis/congifserver/CongifServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.mvr.dcis.congifserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongifServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
